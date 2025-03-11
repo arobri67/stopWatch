fa7eea1ab7497835daacfee36b8d6580ed7043aa
Arnaud <arnaud.obri@proton.me>
2025-03-11 23:02:25 +0100

    add logic for lap time

--- src/main.js
+++ src/main.js
@@ -4,9 +4,14 @@ let initialTime;
 let elapsedTime = 0;
 let timerInterval;
 
+let laps = [];
+let lapCounter = 0;
+
 const display = document.querySelector(".stopwatch-display");
 const startAndStopBtn = document.getElementById("start-stop");
 const lapAndResetBtn = document.getElementById("lap-reset");
+
+const lapsList = document.querySelector(".laps-list");
 
 function startStop() {
   if (timerInterval) {
@@ -18,7 +23,7 @@ function startStop() {
   }
   else {
     initialTime = performance.now();
-    timerInterval = setInterval(renderTime, 1);
+    timerInterval = setInterval(updateTime, 1);
     startAndStopBtn.textContent = "Stop";
     lapAndResetBtn.textContent = "Lap";
   }
@@ -26,7 +31,9 @@ function startStop() {
 
 function lapReset() {
   if (timerInterval) {
-    renderLaps();
+    lapRecorder();
+    const deleteLapBtn = document.querySelector(".delete-lap");
+    deleteLapBtn.addEventListener("click", removeLap);
   }
   else {
     resetAll();
@@ -34,24 +41,71 @@ function lapReset() {
 }
 
 function resetAll() {
-  // todo
+  clearInterval(timerInterval);
+  timerInterval = null;
+  elapsedTime = 0;
+  display.textContent = "00:00:00.00";
+
+  laps = [];
+  lapCounter = 0;
+  lapsList.innerHTML = "";
 }
 
-function renderLaps() {
-// todo
+function removeLap() {
+  const parent = document.parentElement;
+  console.log(parent);
 }
 
-function renderTime() {
+function lapRecorder() {
+  lapCounter++;
+  const currentTime = performance.now();
+  const totalTime = currentTime - initialTime;
+  laps.push({
+    number: lapCounter,
+    total: timeFormater(totalTime),
+  });
+
+  const lapItem = document.createElement("li");
+  lapItem.className = "lap-item";
+  lapItem.id = `lap${lapCounter}`;
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "X";
+  deleteButton.className = "delete-lap";
+  const lapNumber = document.createElement("span");
+  lapNumber.textContent = `Lap ${lapCounter}`;
+  const lapTime = document.createElement("span");
+  lapTime.textContent = `${timeFormater(totalTime)}`;
+
+  // lapItem.innerHTML = `
+  // <button>X</button>
+  // <span class="lap-number">Lap ${lapCounter}</span>
+  // <span class="lap-time">${timeFormater(totalTime)}</span>
+  //  `;
+  lapItem.append(deleteButton);
+  lapItem.append(lapNumber);
+  lapItem.append(lapTime);
+  lapsList.insertBefore(lapItem, lapsList.firstChild);
+}
+
+function updateTime() {
   const currentTime = performance.now();
   const totalTimeElapsed = elapsedTime + (currentTime - initialTime);
   display.textContent = timeFormater(totalTimeElapsed);
 }
 
 function timeFormater(t) {
-  const hours = Math.floor(t / 3600000).toString().padStart(2, "0");
-  const minutes = Math.floor((t % 3600000) / 60000).toString().padStart(2, "0");
-  const seconds = Math.floor((t % 60000) / 1000).toString().padStart(2, "0");
-  const millisecondes = Math.floor(t % 1000).toString().padStart(3, "0");
+  const hours = Math.floor(t / 3600000)
+    .toString()
+    .padStart(2, "0");
+  const minutes = Math.floor((t % 3600000) / 60000)
+    .toString()
+    .padStart(2, "0");
+  const seconds = Math.floor((t % 60000) / 1000)
+    .toString()
+    .padStart(2, "0");
+  const millisecondes = Math.floor(t % 1000)
+    .toString()
+    .padStart(3, "0");
 
   return `${hours}:${minutes}:${seconds}.${millisecondes}`;
 }
